c66b219521db03bd972f2c2f2c754bffb3b9ec5b
sourabh chavan <spc4700@gmail.com>
2023-07-16 12:43:45 +0530

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = "MY NAME IS SOURABH P C";
+
+            double averageLength = CalculateAverageWordLength(input);
+            double roundedAverage = Math.Round(averageLength, 3);
+
+            Console.WriteLine($"Average word length: {roundedAverage}");
+        }
+
+        public static double CalculateAverageWordLength(string input)
+        {
+            string[] words = input.Split(' ');
+            int totalLength = 0;
+
+            foreach (string word in words)
+            {
+                totalLength += word.Length;
+            }
+
+            return (double)totalLength / words.Length;
+        }
+    }
+}
